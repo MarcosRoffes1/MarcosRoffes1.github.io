19a9cdd7aac39eb485d3940b4dfa9cdfc0f68219
MarcosRoffes1 <130808336+MarcosRoffes1@users.noreply.github.com>
2023-11-26 19:15:39 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Formulario from './components/Formulario'
+import './components/Formulario.css'
+import './App.css';
+
+
+
+
+  
+
+
+function App() {
+  return (
+    <div>
+      
+      <Formulario></Formulario>
+    </div>
+  );
+}
+
+export default App;
+ 
